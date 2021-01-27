13c346ee52f1d0861104116eefc3abe474db610d
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2021-01-28 07:44:06 +0900

    SyntaxError対応(end 抜け)

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Cookie
     config.generators do |g|
       g.assets false
       g.helper false
+    end
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
