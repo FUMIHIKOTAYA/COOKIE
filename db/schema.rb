efc2d2e9a7906ecbf6c4265080f64cf996a72940
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2021-01-29 08:03:13 +0900

    postモデルとテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_214735) do
+ActiveRecord::Schema.define(version: 2021_01_28_225147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "restaurant_name", null: false
+    t.string "food_genre", null: false
+    t.text "note"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
