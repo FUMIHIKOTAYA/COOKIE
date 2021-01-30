f046f5af157a56e61ba204cbab18a1a96bb944c2
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2021-01-30 09:44:28 +0900

    Post・Orderモデルでバリデーションを定義

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :post, inverse_of: :orders
+
+  validates :recommend_dish, presence: true, length: { maximum: 50 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   has_many :orders, dependent: :destroy, inverse_of: :post
   accepts_nested_attributes_for :posts, allow_destroy: true
+
+  validates :restaurant_name, presence: true, length: { maximum: 50 }
+  validates :food_genre, presence: true
 end
