3a63db2d1b86d3e80df4e79cf462a5a26a46c76c
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2021-02-04 07:05:00 +0900

    user・postのモデル間の関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_224458) do
+ActiveRecord::Schema.define(version: 2021_02_03_214716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2021_02_01_224458) do
     t.text "note"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,4 +47,5 @@ ActiveRecord::Schema.define(version: 2021_02_01_224458) do
   end
 
   add_foreign_key "orders", "posts"
+  add_foreign_key "posts", "users"
 end
