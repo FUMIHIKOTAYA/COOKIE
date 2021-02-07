a58149041f590ffc51af5974e84b049335c2a6ab
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2021-02-07 14:07:25 +0900

    フォロー機能に必要なメソッドをuserモデルに定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,16 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, on: :create
 
   mount_uploader :profile_image, ProfileImageUploader
+
+  def follow!(other_user)
+    active_relationships.create!(followed_id: other_user.id)
+  end
+
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
 end
