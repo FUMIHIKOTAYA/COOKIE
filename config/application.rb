6dd97e8e9dd430b8ab0c7995b97596a912a409de
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2021-02-14 23:17:38 +0900

    RSpecに必要なgemをインストール、RSpecの初期設定

--- config/application.rb
+++ config/application.rb
@@ -7,13 +7,20 @@ Bundler.require(*Rails.groups)
 module Cookie
   class Application < Rails::Application
     config.load_defaults 5.2
-    config.generators do |g|
-      g.assets false
-      g.helper false
-    end
-
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
     # config.active_record.default_timezone = :local
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
+    end
   end
 end
