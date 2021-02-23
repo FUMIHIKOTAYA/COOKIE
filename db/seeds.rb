f7b9e58fb3abd8a5550e66c0870ef53bc2603d08
FUMIHIKO TAYA <tayafumihiko@gmail.com>
2021-02-24 08:22:20 +0900

    seedデータ数(post)・各viewsのアイコンサイズを変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ require 'faker'
   name = Faker::JapaneseMedia::StudioGhibli.character
   email = Faker::Internet.email
   password = "password"
-  profile = Faker::TvShows::Friends.quote
+  profile = Faker::TvShows::Suits.quote
   profile_image = open("./db/fixtures/icon#{n}.jpeg")
   User.create!(
     name: name,
@@ -16,7 +16,7 @@ require 'faker'
   )
 end
 
-20.times do |n|
+30.times do |n|
   restaurant_name = Faker::Restaurant.name
   food_genre = rand(19)
   note = Faker::Restaurant.review
@@ -33,7 +33,7 @@ end
   recommend_dish = Faker::Food.dish
   reason = Faker::Food.description
   dish_image = open("./db/fixtures/img#{n}.jpeg")
-  post_id = rand(1..20)
+  post_id = rand(1..30)
   Order.create!(
     recommend_dish: recommend_dish,
     reason: reason,
